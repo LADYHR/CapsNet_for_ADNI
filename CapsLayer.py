cedb2008cfa5b346fe7a322d9c1971258f68c5b3
LADYHR <ladyhr@outlook.com>
2018-04-02 18:37:01 +0800

    add capslayer

--- CapsLayer.py
+++ CapsLayer.py
@@ -11,3 +11,47 @@ Abstract: This file is used to build a Capsule layer.
 '''
 #!user/bin/env python
 # -*- coding: utf-8 -*-
+
+import numpy as np
+import  tensorflow as tf
+
+from config import  cfg
+
+epsilon = 1e-9
+
+class CapsLayer(object): # class CapsLayer inherit class object
+	def __init__(self, caps_num, num_outputs, vec_len, with_routing=True, layer_type='FC'):
+		self.caps_num = caps_num
+		self.num_outputs = num_outputs
+		self.vec_len = vec_len
+		self.with_routing = with_routing
+		self.layer_type = layer_type
+
+	def __call__(self, input, kernel_size=None, stride=None):
+		if self.layer_type == 'CONV':
+			self.kernel_size = kernel_size
+			self.stride = stride
+
+			if not self.with_routing:
+				# assert input.get_shape() == [cfg.batch_size, 89, 89, 128]
+				capsules = tf.contrib.layers.conv2d(input, self.num_outputs * vec_len,
+													self.kernel_size, self.stride, padding='VALID',
+													activation_fn=tf.nn.relu)
+				capsules = tf.reshape(capsules, (cfg.batch_size, -1, self.vec_len, 1))
+
+				capsules = squash(capsules)
+				# assert capsules.getshape() == [cfg.batch_size, self.caps_num, 8, 1]
+				return(capsules)
+
+		if self.layer_type == 'FC':
+			if self.with_routing:
+				self.input = tf.reshape(input, shape=(cfg.batch_size, -1, 1, input.shape[-2].value, 1))
+				with tf.variable_scope('routing'):
+					b_IJ = tf.constant(np.zeros([cfg.batch_size, input.shape[1].value, self.num_outputs, 1, 1], dtype=np.float32))
+					capsules = routing(self.input, b_IJ)
+					capsules = tf.squeeze(capsules, axis=1)
+
+			return(capsules)
+
+def routing(input, b_IJ):
+	W = tf.get_variable()
--- CapsNet.py
+++ CapsNet.py
@@ -6,7 +6,7 @@ All rights reserved
 
 FileName: CapsNet.py
 Abstract: This file defines the structure of the
-Capsule Network.
+Capsule Network. And add summary to TensorBoard.
 =====================================================
 =====================================================
 '''
@@ -34,18 +34,63 @@ class CapsNet(object):
 
 				self.global_step = tf.Variable(0, name='global_step', trainable=False)
 				self.optimizer = tf.train.AdamOptimizer()
-				self.train_op = self.optimizer.minimize(self.total_loss, global_step=self.global_step)
+				self.train_op = self.optimizer.minimize(self.margin_loss, global_step=self.global_step)
 			else:
-				self.X = tf.placeholder(tf.float32,shape=(cfg.batch_size, 28, 28, 1))
+				self.X = tf.placeholder(tf.float32,shape=(cfg.batch_size, 91, 91, 1))
 				self.labels = tf.placeholder(tf.int32, shape=(cfg.batch_size,1))
 				self.Y = tf.reshape(self.labels, shape=(cfg.batch_size, 3,1))
-				self.build_arch
+				self.build_arch()
+
 
 		tf.logging.info('Setting up the main structure')
 
 	def build_arch(self):
 		with tf.variable_scope('Conv1_layer'):
-			conv1 = tf.layers.conv2d()
+			#  conv1, [batch_size, 91, 91, 100]
+			conv1 = tf.contrib.layers.conv2d(self.X, num_outputs=128, kernel_size=3,
+											 stride=1, padding='VALID'_)
+			assert conv1.get_shape() == [cfg.batch_size, 89, 89, 128]
+
+		with tf.variable_scope('PrimaryCaps_layer'):
+			#  Primary Capsule layer, [batch_size, 41*41*16,8,1]
+			primaryCaps = CapsLayer(caps_num=26896, num_outputs=16,vec_len=8,with_routing=False,layer_type='CONV')
+			caps1 = primaryCaps(conv1, kernel_size=9, stride=2)
+			assert  caps1.get_shape() == [cfg.batch_size, 26896, 8, 1]
+			#  Primary Capsule layer2, [batch_size, 17*17*16,8,1]
+			primaryCaps2 = CapsLayer(caps_num=4624, num_outputs=16, vec_len=8, with_routing=True, layer_type='CONV')
+			caps2 = primaryCaps2(caps1, kernel_size=9, stride=2)
+			assert caps2.get_shape() == [cfg.batch_size, 4624, 8, 1]
+
+		with tf.variable_scope('DigitCaps_layer'):
+			digitCaps = CapsLayer(caps_num=3, num_outputs=3, vec_len=16, with_routing=True, layer_type='FC')
+			self.caps3 = digitCaps(caps2)
+			# assert caps3.get_shape() == [cfg.batch_size, 3, 16, 1]???
+
+	def loss(self):
+		#  calculate ||v_c||, then do softmax and calculate the predicted labels
+		self.v_length = tf.sqrt(tf.reduce_sum(tf.square(self.caps3),axis=2,keep_dims=True)+epsilon) #  axis=2 ???
+		self.softmax_v = tf.nn.softmax(self.v_length,dim=1)
+		assert self.softmax_v.get_shape() == [cfg.batch_size, 3, 1, 1]
+		self.argmax_idx = tf.to_int32(tf.argmax(self.softmax_v,axis=1))
+		assert self.argmax_idx.get_shape() == [cfg.batch_size, 1, 1]
+		self.argmax_idx = tf.reshape(self.argmax_idx, shape=(cfg.batch_size,))
+
+		#  calculate the margin loss
+		max_l = tf.square(tf.maximum(0., cfg.m_plus - self.v_length))
+		max_r = tf.square(tf.maximum(0., self.v_length - cfg.m_minus))
+		assert max_l.get_shape() == [cfg.batch_size, 3, 1, 1]
+		T_c = self.Y
+		L_c = T_c * max_l + cfg.lambdal * (1-T_c) * max_r
+		self.margin_loss = tf.reduce_mean(tf.reduce_sum(L_c, axis=1))
+
+	def _summary(self): #  start with _ means this func is a internal func
+		train_summary = []
+		train_summary.append(tf.summary.scalar('train/margin_loss',self.margin_loss))
+		self.train_summary = tf.summary.merge(train_summary)
+
+		correct_prediction = tf.equal(tf.to_int32(self.labels),self.argmax_idx)
+		self.accuracy = tf.reduce_sum(tf.cast(correct_prediction, tf.float32))
+
 
 
 
