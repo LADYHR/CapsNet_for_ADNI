0d70a6676d7c92b3c81e5523658d74792a37219b
LADYHR <ladyhr@outlook.com>
2018-04-23 16:11:46 +0800

    add TFReconds file

--- utils.py
+++ utils.py
@@ -10,10 +10,34 @@ batch data.
 =====================================================
 =====================================================
 '''
-#!user/bin/env python
+# !user/bin/env python
 # -*- coding: utf-8 -*-
 
 import tensorflow as tf
 
+def read_and_decode(filename):
+	# 根据文件名生成一个文件队列
+	filename_queue = tf.train.string_input_producer([filename])
+	#  根据文件格式选择对应的文件阅读器，创建reader
+	reader = tf.TFRecordReader()
+	# 从文件队列中读取一个序列化的样本
+	key, serialized_example = reader.read(filename_queue)
+	#  解析序列化的样本，提取特征
+	features = tf.parse_single_example(serialized_example,
+	                                   features={
+		                                   'label': tf.FixedLenFeature([], tf.int64),
+		                                   'img_raw': tf.FixedLenFeature([], tf.string),
+	                                   })
+	img = tf.decode_raw(features['img_raw'], tf.uint8)
+	img = tf.cast(img, tf.float32) * (1. / 255) - 0.5
+	label = tf.cast(features['label'], tf.int32)
+
+	return img, label
+
 def get_batch_data(dataset, batch_size, num_threads):
 	pass
+
+if __name__ == "__main__":
+	img, label = read_and_decode("train.tfrecords")
+	print(img, label)
+	print("Finished!")
--- write_to_tfrecords.py
+++ write_to_tfrecords.py
@@ -10,7 +10,7 @@ batch data.
 =====================================================
 =====================================================
 '''
-#!user/bin/env python
+# !user/bin/env python
 # -*- coding: utf-8 -*-
 
 import tensorflow as tf
@@ -18,7 +18,7 @@ import os
 from PIL import Image
 
 
-rootpath = "H:\\ADNI_DATA\\TEST\\Axial_45"
+rootpath = "C:\\Users\\LADYHR\\Git_Repository\\CapsNet_for_ADNI\\TEST\\Axial_45"
 '''
 AD -- img1.png
       img2.png
@@ -32,18 +32,19 @@ NL -- img1.png
       ...
 '''
 
-classes = ['AD', 'MCI', "NL"]
+classes = ['AD', 'MCI', 'NL']
 writer = tf.python_io.TFRecordWriter("train.tfrecords")
 for index, name in enumerate(classes, start=1):
-	class_path = rootpath + name + "/"
-	for img_path in os.listdir(class_path):
-		img = Image.open(img_path)
-		img_raw = img.tobytes()  # 图片转化为原生bytes
-		example = tf.train.Example(features=tf.train.Features(feature={
-			"label": tf.train.Feature(int64_list=tf.train.Int64List(value=[index])),  # 定义数据格式中的标签
-			"img_raw": tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))  # 定义数据格式中的数据
-		}))
-		writer.write(example.SerializeToString)  # 序列化为字符串
+    class_path = rootpath + '\\' + name
+    for img_file in os.listdir(class_path):
+        img_path = os.path.join(class_path, img_file)
+        img = Image.open(img_path)
+        img_raw = img.tobytes()
+        print(index, img_raw)
+        example = tf.train.Example(features=tf.train.Features(feature={
+            "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[index])),   # 定义数据格式中的标签
+            "img_raw": tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))}))  # 定义数据格式中的数据
+        writer.write(example.SerializeToString())  # 序列化为字符串
 writer.close()
 
 
